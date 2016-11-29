2e1503fd0644c55fb5d76dc4a64cb2a2d3e80de3
sewan <kido184@hotmail.com>
2016-11-29 22:57:32 +0000

    hello

--- DBCONNECT.php
+++ DBCONNECT.php
@@ -5,7 +5,7 @@ define("DB_PASS", "360b7be5");
 define("DB_NAME", "cm3028rgu");
 
 // 1. Create a database connection
-$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+$db = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 // Test if connection succeeded
 if(mysqli_connect_errno()) {
     die("Database connection failed: " .

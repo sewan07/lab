556eb6fdc275ac2449324cf9fa75d1eaa7c1a6ea
sewan <kido184@hotmail.com>
2016-11-29 23:05:55 +0000

    hello

--- DBCONNECT.php
+++ DBCONNECT.php
@@ -13,3 +13,22 @@ if($db->connect_errno){
 die('Connectfailed['.$db->connect_error.']');
 }
 
+
+// create a SQL query as a string
+$sql_query = "SELECT * FROM superheros WHERE superpower LIKE '%l aser%' ";
+// execute the SQL query
+$result = $db->query($sql_query);
+
+// iterate over $resul t obj ect one $row at a time
+// use fetch_array() to return an associ ative array
+while($row = $result->fetch_array()){
+// process the $row
+}
+
+while($row = $result->fetch_array()){
+// print out fiel ds from row of data
+    echo "<p> ’ ’ . $row ['superheroName'].‘ ‘</p>";
+}
+
+$result->close();
+// cl ose connection to database

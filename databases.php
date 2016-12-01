255f7fde50f984cb072edd145cf338758b0559d7
sewan <kido184@hotmail.com>
2016-12-01 08:03:50 +0000

    hello

--- databases.php
+++ databases.php
@@ -15,15 +15,18 @@ if(mysqli_connect_errno()) {
 }
 ?>
 
+<?php
+$query = "SELECT * FROM marvelmovies";
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title> portlethen </title>
 </head>
 <body>
-<p>
-    <a href="About.php">Second Page</a>
-</p>
+
 </body>
 </html>
 

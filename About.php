218070b3e383be0428843c29af9d4914682a1656
sewan <kido184@hotmail.com>
2016-12-01 07:39:40 +0000

    hello

--- About.php
+++ About.php
@@ -8,8 +8,8 @@
 <?php $link_name = "About Page"; ?>
 <?php $id = 2; ?>
 
-<?php $link_name = "About Page"; ?>
-<a href="About.php?id=1"> <?php echo $id; ?>"> <?php echo $link_name; ?></a>
+
+<a href="About.php?id= <?php echo $id; ?>"> <?php echo $link_name; ?></a>
 
 </body>
 </html>

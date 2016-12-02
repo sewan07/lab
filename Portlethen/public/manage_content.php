180dc8aa98c569e3ae0255c5f2c87d8eb4299794
sewan <kido184@hotmail.com>
2016-12-02 17:28:43 +0000

    hello

--- Portlethen/public/manage_content.php
+++ Portlethen/public/manage_content.php
@@ -1,6 +1,6 @@
 
 <?php require_once("../includes/db_connection.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
+<?php require_once("../includes/function.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
 
 <?php

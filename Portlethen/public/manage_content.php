085777492e6ccdde23a1d64111b37b2ad0759365
sewan <kido184@hotmail.com>
2016-12-02 17:31:33 +0000

    hello

--- Portlethen/public/manage_content.php
+++ Portlethen/public/manage_content.php
@@ -3,36 +3,37 @@
 <?php require_once("../includes/function.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
 
-<?php
-if (isset($_GET["subjects"])) {
-    $selected_subject_id = $_GET["subjects"];
-    $selected_page_id = null;
-} elseif (isset($_GET["pages"])) {
-    $selected_subject_id = null;
-    $selected_page_id = $_GET["pages"];
-} else {
-    $selected_subject_id = null;
-    $selected_page_id = null;
-}
-
-?>
 <div id="main">
     <div id="navigation">
         <ul class="subjects">
-            <?php $subject_set = find_all_subjects(); ?>
+            <?php
+            $query  = "SELECT * ";
+            $query .= "FROM subjects ";
+            $query .= "WHERE visible = 1 ";
+            $query .= "ORDER BY position ASC";
+            $subject_set = mysqli_query($connection, $query);
+            confirm_query($subject_set);
+            ?>
             <?php
             while($subject = mysqli_fetch_assoc($subject_set)) {
                 ?>
                 <li>
-                    <a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
-                    <?php $page_set = find_pages_for_subject($subject["id"]); ?>
+                    <a href="manage_content.php?subject=<?php echo urlencode( $subject["id"]); ?> "><?php echo $subject["menu_name"]; ?></a>
+                    <?php
+                    $query  = "SELECT * ";
+                    $query .= "FROM pages ";
+                    $query .= "WHERE visible = 1 ";
+                    $query .= "AND subject_id = {$subject["id"]} ";
+                    $query .= "ORDER BY position ASC";
+                    $page_set = mysqli_query($connection, $query);
+                    confirm_query($page_set);
+                    ?>
                     <ul class="pages">
                         <?php
                         while($page = mysqli_fetch_assoc($page_set)) {
                             ?>
                             <li>
-                                <a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
-                            </li>
+                                <a href="manage_content.php?page=<?php echo urlencode( $page["id"]); ?>"><?php echo $page["menu_name"]; ?></li></a>
                             <?php
                         }
                         ?>
@@ -47,9 +48,10 @@ if (isset($_GET["subjects"])) {
     </div>
     <div id="page">
         <h2>Manage Content</h2>
-        <?php echo $selected_subject_id; ?><br />
-        <?php echo $selected_page_id; ?>
+
     </div>
 </div>
 
 <?php include("../includes/layouts/footer.php"); ?>
+
+

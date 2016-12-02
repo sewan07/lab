d9dbf2381656d68c16d86339fd21c8106f5d6ac2
sewan <kido184@hotmail.com>
2016-12-02 16:35:27 +0000

    hello

--- Portlethen/public/manage_content.php
+++ Portlethen/public/manage_content.php
@@ -7,69 +7,49 @@
     <div id="navigation">
         <ul class="subjects">
             <?php
-            $query = "SELECT * ";
+            $query  = "SELECT * ";
             $query .= "FROM subjects ";
             $query .= "WHERE visible = 1 ";
             $query .= "ORDER BY position ASC";
             $subject_set = mysqli_query($connection, $query);
-            //Test if there was a query error
             confirm_query($subject_set);
             ?>
             <?php
-            // Use returnes data if any
             while($subject = mysqli_fetch_assoc($subject_set)) {
-
                 ?>
                 <li>
-                    <?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?>
+                    <?php echo $subject["menu_name"]; ?>
                     <?php
-                    $query = "SELECT * ";
+                    $query  = "SELECT * ";
                     $query .= "FROM pages ";
                     $query .= "WHERE visible = 1 ";
                     $query .= "AND subject_id = {$subject["id"]} ";
                     $query .= "ORDER BY position ASC";
                     $page_set = mysqli_query($connection, $query);
-                    //Test if there was a query error
                     confirm_query($page_set);
                     ?>
-                <ul class="pages">
-                    <?php
-                    // Use returnes data if any
-                    while($page = mysqli_fetch_assoc($page_set)) {
-
-                    ?>
-                </ul>
-                    <li>
-                        <?php echo $page["menu_name"]; ?> </li>
+                    <ul class="pages">
                         <?php
-                    }
-                    ?>
-                    <?php
-                    //release returned data
-                    mysqli_free_result($page_set);
-                    ?>
-
-                </ul>
-
+                        while($page = mysqli_fetch_assoc($page_set)) {
+                            ?>
+                            <li><?php echo $page["menu_name"]; ?></li>
+                            <?php
+                        }
+                        ?>
+                        <?php mysqli_free_result($page_set); ?>
+                    </ul>
                 </li>
-
                 <?php
             }
             ?>
-            <?php
-            //release returned data
-            mysqli_free_result($subject_set);
-            ?>
+            <?php mysqli_free_result($subject_set); ?>
         </ul>
-
     </div>
     <div id="page">
         <h2>Manage Content</h2>
 
     </div>
 </div>
-
-
 
 <?php include("../includes/layouts/footer.php"); ?>
 
